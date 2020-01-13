01f527e3c2ded164cfac07a1c9654800d521ed09
Solomiia Sahala <35579102+solomiia-sahala@users.noreply.github.com>
2020-01-13 23:55:04 +0200

    Update index.js

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -107,7 +107,7 @@ getPastDay(date, 365);
 //Task 10
  function formatDate(date) {
      let year = date.getFullYear();
-     let month = date.getMonth();
+     let month = date.getMonth() + 1;
      let day = date.getDate();
      let hours = date.getHours() < 10 ? `0${date.getHours()}`: date.getHours();
      let minutes = date.getMinutes() < 10 ? `0${date.getMinutes()}`: date.getMinutes();
